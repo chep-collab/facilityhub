6c649ff92290fe7f791dddf2d4953eeac815bf5e
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-03-24 20:52:03 +0100

    home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-      <br>
-      <AppAlert>
-        This is an auto-imported component
-        <UButton color="primary" variant="solid">Button</UButton>
-        </AppAlert>
-    </div>
-  </template>
+  <div>
+    <UCard>
+      Workspace Admin
+      <template #footer>
+        <div class="text-xs">You own a workspace or study center</div>
+      </template>
+    </UCard>
+    <br />
+    <UCard>
+      Workspace User
+      <template #footer>
+        <div class="text-xs">Find and subscribe to workspaces</div>
+      </template>
+    </UCard>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
