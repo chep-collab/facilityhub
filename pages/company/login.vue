bb14322fa6201fcaa0bece0f9e5795d1f63cddea
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-03-24 22:21:07 +0100

    login and signup forms

--- pages/company/login.vue
+++ pages/company/login.vue
@@ -1,8 +1,53 @@
+<script setup lang="ts">
+import { object, string, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+
+const schema = object({
+  email: string().email("Invalid email").required("Required"),
+  password: string()
+    .min(8, "Must be at least 8 characters")
+    .required("Required"),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with event.data
+  console.log(event.data);
+}
+</script>
+
 <template>
+  <UCard>
+    <template #header> Admin Login </template>
     <div>
-      <h1>Company Login</h1>
-      <AppAlert>
-        This is an auto-imported component
-      </AppAlert>
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Password" name="password">
+          <UInput v-model="state.password" type="password" />
+        </UFormGroup>
+
+        <UButton type="submit" block> Login </UButton>
+      </UForm>
     </div>
-  </template>
+    <template #footer>
+      <div class="flex flex-row justify-around gap-8">
+        <ULink to="/company/signup">Signup</ULink>
+        <ULink>Forgot Password</ULink>
+      </div>
+    </template>
+  </UCard>
+</template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,29 +3,29 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
   theme: {
     extend: {
-      card: {
-        base: "",
-        background: "bg-white dark:bg-gray-900",
-        divide: "divide-y divide-gray-200 dark:divide-gray-800",
-        ring: "ring-1 ring-gray-200 dark:ring-gray-800",
-        rounded: "rounded-lg",
-        shadow: "shadow",
-        body: {
-          base: "",
-          background: "",
-          padding: "px-4 py-2 sm:p-6",
-        },
-        header: {
-          base: "",
-          background: "",
-          padding: "px-4 py-5 sm:px-6",
-        },
-        footer: {
-          base: "",
-          background: "",
-          padding: "px-4 py-2 sm:px-6",
-        },
-      },
+      // card: {
+      //   base: "",
+      //   background: "bg-white dark:bg-gray-900",
+      //   divide: "divide-y divide-gray-200 dark:divide-gray-800",
+      //   ring: "ring-1 ring-gray-200 dark:ring-gray-800",
+      //   rounded: "rounded-lg",
+      //   shadow: "shadow",
+      //   body: {
+      //     base: "",
+      //     background: "",
+      //     padding: "px-4 py-2 sm:p-6",
+      //   },
+      //   header: {
+      //     base: "",
+      //     background: "",
+      //     padding: "px-4 py-5 sm:px-6",
+      //   },
+      //   footer: {
+      //     base: "",
+      //     background: "",
+      //     padding: "px-4 py-2 sm:px-6",
+      //   },
+      // },
     },
   },
   plugins: [],
