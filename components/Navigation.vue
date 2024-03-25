9548f403c5bad02e039a4fc350797a90e4e43d9f
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-03-25 22:38:10 +0100

    updated navigations

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,32 +1,43 @@
 <script setup lang="ts">
 const route = useRoute();
+const activeUser = useActiveUser();
 
-const links = [
-  [
-    {
-      label: "Services",
-      icon: "i-heroicons-cog-6-tooth",
-      to: "/company/dashboard/home",
-    },
-    {
-      label: "Subscriptions",
-      icon: "i-heroicons-credit-card",
-      to: "/company/dashboard/subscriptions",
-    },
-    {
-      label: "Users",
-      icon: "i-heroicons-user",
-      to: "/company/dashboard/users",
-    },
-  ],
-  //  [{
-  //   label: 'Profile',
-  //   icon: 'i-heroicons-light-bulb'
-  // }, {
-  //   label: 'Logout',
-  //   icon: 'i-heroicons-question-mark-circle'
-  // }]
+const companyTabs = [
+  {
+    label: "Services",
+    icon: "i-heroicons-cog-6-tooth",
+    to: "/dashboard/home",
+  },
+  {
+    label: "Subscriptions",
+    icon: "i-heroicons-credit-card",
+    to: "/dashboard/subscriptions",
+  },
+  {
+    label: "Users",
+    icon: "i-heroicons-user",
+    to: "/dashboard/users",
+  },
 ];
+
+const userTabs = [
+  {
+    label: "My Subscriptions",
+    icon: "i-heroicons-user",
+    to: "/dashboard/my-subscriptions",
+  },
+  {
+    label: "My Centers",
+    icon: "i-heroicons-user",
+    to: "/dashboard/joined-centers",
+  },
+];
+const links =
+  activeUser === "company"
+    ? companyTabs
+    : activeUser === "user"
+    ? userTabs
+    : [];
 </script>
 
 <template>
