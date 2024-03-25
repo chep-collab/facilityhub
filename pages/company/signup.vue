4bfb7023c91aba19a788d7dc5f9242c1788b31f7
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-03-25 20:08:54 +0100

    company signup api int

--- pages/company/signup.vue
+++ pages/company/signup.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { object, string, type InferType } from "yup";
+import { object, string, number, type InferType } from "yup";
 import type { FormSubmitEvent } from "#ui/types";
+import { handleErrorMessages } from "../../common/errorHandlers";
+
+const runtimeConfig = useRuntimeConfig();
+const toast = useToast();
+const router = useRouter();
 
 const schema = object({
+  name: string().required().min(2),
+  phone: number().required(),
+  address: string().required().min(2),
   email: string().email("Invalid email").required("Required"),
   password: string()
     .min(8, "Must be at least 8 characters")
@@ -11,17 +19,46 @@ const schema = object({
 
 type Schema = InferType<typeof schema>;
 
+const pending = ref(false);
 const state = reactive({
+  name: undefined,
   email: undefined,
   password: undefined,
-  name: undefined,
   phone: undefined,
   address: undefined,
 });
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  // Do something with event.data
-  console.log(event.data);
+  try {
+    pending.value = true;
+    const { data, error } = await useFetch("/company/signup", {
+      method: "POST",
+      body: {
+        name: state.name,
+        phone: state.phone,
+        address: state.address,
+        email: state.email,
+        password: state.password,
+      },
+      baseURL: runtimeConfig.public.apiUrl,
+    });
+    if (error.value) throw error.value.data.message;
+    toast.add({
+      title: "Company account created. You will be taken to login",
+    });
+    setTimeout(() => {
+      router.push("/company/login");
+    }, 1500);
+  } catch (error: any) {
+    if (error) {
+      toast.add({
+        title: handleErrorMessages(error),
+        color: "red",
+      });
+    }
+  } finally {
+    pending.value = false;
+  }
 }
 </script>
 
@@ -29,12 +66,16 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   <UCard>
     <template #header> Admin Signup </template>
     <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-      <UFormGroup label="Company name" name="text">
+      <UFormGroup label="Company name" name="name">
         <UInput v-model="state.name" />
       </UFormGroup>
 
       <UFormGroup label="Email" name="email">
         <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Address" name="address">
+        <UInput v-model="state.address" />
       </UFormGroup>
 
       <UFormGroup label="Phone number" name="phone">
@@ -45,7 +86,9 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
         <UInput v-model="state.password" type="password" />
       </UFormGroup>
 
-      <UButton type="submit" block> Signup </UButton>
+      <UButton :loading="pending" :disabled="pending" type="submit" block>
+        Signup
+      </UButton>
     </UForm>
     <template #footer>
       <div class="flex flex-row justify-around gap-8">
