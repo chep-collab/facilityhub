9afe2828a0098d64fb083412ae1f126aa9d9eb51
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-03-25 19:57:43 +0100

    user signup api Int

--- pages/user/signup.vue
+++ pages/user/signup.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import { object, string, type InferType } from "yup";
+import { object, string, type InferType, number } from "yup";
 import type { FormSubmitEvent } from "#ui/types";
+import { handleErrorMessages } from "../../common/errorHandlers";
+const runtimeConfig = useRuntimeConfig();
 
+const toast = useToast();
+const router = useRouter();
 const schema = object({
-  email: string().email("Invalid email").required("Required"),
+  firstName: string().required().min(2),
+  lastName: string().required().min(2),
+  phone: number().required(),
+  email: string().email("This is compulsory").required("Required"),
   password: string()
     .min(8, "Must be at least 8 characters")
     .required("Required"),
@@ -18,10 +25,38 @@ const state = reactive({
   phone: undefined,
   password: undefined,
 });
-
+const pending = ref(false);
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  // Do something with event.data
-  console.log(event.data);
+  try {
+    pending.value = true;
+    const { data, error } = await useFetch("/user/signup", {
+      method: "POST",
+      body: {
+        firstName: state.firstName,
+        lastName: state.lastName,
+        phone: state.phone,
+        email: state.email,
+        password: state.password,
+      },
+      baseURL: runtimeConfig.public.apiUrl,
+    });
+    if (error.value) throw error.value.data.message;
+    toast.add({
+      title: "Account created. You will be taken to login",
+    });
+    setTimeout(() => {
+      router.push("/user/login");
+    }, 1500);
+  } catch (error: any) {
+    if (error) {
+      toast.add({
+        title: handleErrorMessages(error),
+        color: "red",
+      });
+    }
+  } finally {
+    pending.value = false;
+  }
 }
 </script>
 
@@ -29,11 +64,11 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   <UCard>
     <template #header> User Signup </template>
     <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-      <UFormGroup label="First Name" name="text">
+      <UFormGroup label="First Name" name="firstName">
         <UInput v-model="state.firstName" />
       </UFormGroup>
 
-      <UFormGroup label="Last Name" name="text">
+      <UFormGroup label="Last Name" name="lastName">
         <UInput v-model="state.lastName" />
       </UFormGroup>
 
@@ -42,14 +77,16 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
       </UFormGroup>
 
       <UFormGroup label="Phone number" name="phone">
-        <UInput v-model="state.phone" />
+        <UInput v-model="state.phone" type="phone" />
       </UFormGroup>
 
       <UFormGroup label="Password" name="password">
         <UInput v-model="state.password" type="password" />
       </UFormGroup>
 
-      <UButton type="submit" block> Signup </UButton>
+      <UButton :loading="pending" :disabled="pending" type="submit" block>
+        Signup
+      </UButton>
     </UForm>
     <template #footer>
       <div class="flex flex-row justify-around gap-8">
