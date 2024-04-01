29e7244bce3f660ed56e2d2e227e7fa101994586
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-04-01 11:00:17 +0100

    Update 01.authentication.global.ts

--- middleware/01.authentication.global.ts
+++ middleware/01.authentication.global.ts
@@ -19,4 +19,21 @@ export default defineNuxtRouteMiddleware((to, from) => {
       router.push({ name: "user-login" });
     }
   }
+
+  // note: the if statement below  might need to be modified to seperate between
+  // going to user login page or company login page
+  // for e.g, we might want an authenticated user who clicks on the admin login page to view it
+  // and we might want an authenticated company who clicks on the user login page to view it
+
+  // A simpler approach might be to user a single login form for both user and admin
+  // (Note: the dashboard view takes shapes depending on the user type that is logged in)
+  // If you decide to do the above, then the if statement below will not require modification.
+
+  // Please rememeber to clear out or modify the comment based on what you do.
+  if (
+    destinationName.includes("login") &&
+    getAuthenticationState.value === true
+  ) {
+    router.push({ name: "dashboard" });
+  }
 });
