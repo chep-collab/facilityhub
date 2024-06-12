e60c9892174318c703b1e53da438945d29660484
danielsumah <52764894+danielsumah@users.noreply.github.com>
2024-06-12 14:53:33 +0100

    updated error handler

--- common/errorHandlers.ts
+++ common/errorHandlers.ts
@@ -1,13 +1,13 @@
 import { AxiosError } from "axios";
 
 export const handleErrorMessages = (
-  error: string | string[] | AxiosError
+  error: string | string[] | Error | AxiosError
 ): string => {
   if (typeof error === "string") {
     return error || "An error occurred";
   } else if (Array.isArray(error)) {
     return error.join(", ") || "An error occurred";
-  } else if (error.isAxiosError) {
+  } else if ((error as AxiosError).isAxiosError) {
     const axiosError = error as AxiosError;
     if (
       axiosError.response &&
@@ -18,6 +18,8 @@ export const handleErrorMessages = (
     } else {
       return "An error occurred";
     }
+  } else if (error instanceof Error) {
+    return error.message;
   } else {
     return "An error occurred";
   }
